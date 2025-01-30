778a8236c8289c111c5a8727be40259873e38f28
Calum Courtney <Courtneycd@cardiff.ac.uk>
2025-01-30 09:32:58 +0000

    Health check added

--- main.py
+++ main.py
@@ -71,6 +71,9 @@ def process_sqs_p2_message():
         except Exception as e:
             print(f"An error occurred: {str(e)}")
 
+@app.route('/health', methods=["GET"])
+def health_check():
+    return "Everything is A-OK"
 
 
 if __name__ == '__main__':
