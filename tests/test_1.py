b76aecbcc6e1434e21d8d40aa23e7643a4311ece
Calum-DC <calumcourtney12gmail.com>
2025-03-13 19:12:35 +0000

    tests and ignore

--- tests/test_1.py
+++ tests/test_1.py
@@ -14,13 +14,6 @@ def queue_url_mock(sqs_client):
     return queue_urls
 
 
-@pytest.fixture(scope='function')
-def mock_jira_client():
-    with patch('main.JIRA') as MockJIRA:
-        mock_instance = MockJIRA.return_value 
-        mock_instance.server_info.return_value = {'version': 'mocked_version'} 
-        yield mock_instance
-
 def test_health_check(client):
     """Test to ensure the /health route is working."""
     response = client.get('/health')
